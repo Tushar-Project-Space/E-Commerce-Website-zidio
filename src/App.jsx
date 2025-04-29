ed002d7c7c2b580193499b35ffce1a4a4350a838
Tushar More <158036610+Tushar-Project-Space@users.noreply.github.com>
2025-04-29 10:39:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LandingPage from "./Frontend/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Frontend/LandingPage/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Landing.css";
+const LandingPage = () => {
+  return (
+    <>
+      <h1>E-Commerce Shopping Website.</h1>
+
+      <h2>Team</h2>
+      <ul>
+        <li>Vaishnavi</li>
+        <li>Abhijit</li>
+        <li>Tushar</li>
+      </ul>
+    </>
+  );
+};
+
+export default LandingPage;
